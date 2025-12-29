629444f3b726d143656ebd7305ec621ad7889fdf
Aayushi Savaliya <gopisavaliya001@gmail.com>
2025-12-29 22:51:57 +0530

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './pages/home'
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/navbar'
 import AppRoutes from './Routes'
